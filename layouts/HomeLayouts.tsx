aa7da42979fc5dec690ad3da3964638413f87a94
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-02-14 22:41:14 +0700

    modificate home layout

--- layouts/HomeLayouts.tsx
+++ layouts/HomeLayouts.tsx
@@ -1,8 +1,16 @@
+import Navbar from 'components/organism/Navbar'
 import React from 'react'
 
-const HomeLayouts = () => {
+const HomeLayouts = ({ children }: any) => {
        return (
-              <div>HomeLayouts</div>
+              <React.Fragment>
+                     <div className="max-w-[1440px] mx-auto">
+                            <Navbar />
+                            <div className=" px-4 md:px-32">
+                                   {children}
+                            </div>
+                     </div>
+              </React.Fragment>
        )
 }
 
