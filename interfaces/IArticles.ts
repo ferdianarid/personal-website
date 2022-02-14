939bfdef55dec71d06d3019db598867aa6b42183
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-02-14 22:39:13 +0700

    create experience and article interface

---
+++ interfaces/IArticles.ts
@@ -0,0 +1,8 @@
+export interface IArticles {
+       srcImage: any
+       title: string
+       author: string
+       category: string
+       subtitle: any
+       dateposted: string
+}
---
+++ interfaces/IExperience.ts
@@ -0,0 +1,10 @@
+export interface IExperience {
+       position: string
+       startDate: string
+       endDate: string
+       company: string
+       duration: number
+       workStatus: string
+       location: string
+       description: any
+}
