f9d2eb5659ae565fd2988566b2d34f9d8f7129cb
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-10 08:23:59 +0700

    feature/update layouts

--- layouts/HeaderLayouts.tsx
+++ layouts/HeaderLayouts.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const HeaderLayouts = ({ children }: any) => {
     return (
         <React.Fragment>
-            <div className="cover max-w-[1440px] mx-auto">
+            <div className="cover max-w-[1440px]  mx-auto">
                 {children}
             </div>
         </React.Fragment >
---
+++ layouts/PageLayouts.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from 'components/organism/Navbar'
+import Footer from 'components/templates/Footer'
+
+const PageLayouts = ({ children }: any) => {
+	return (
+		<React.Fragment>
+			<div className="max-w-[1440px] mx-auto">
+				{/* Navbar Section */}
+				<Navbar />
+				{/* Children */}
+				<div className="px-4 md:px-32">
+					{children}
+				</div>
+				{/* Footer Section */}
+				<Footer />
+			</div>
+		</React.Fragment>
+	)
+}
+
+export default PageLayouts
