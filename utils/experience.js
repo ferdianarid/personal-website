0dc721201f2dea1f1c9dd290c4f6fc94722a97d9
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-15 12:43:50 +0700

    update profile about page and navbar

--- utils/experience.js
+++ utils/experience.js
@@ -1,5 +1,15 @@
 export const DataExperience = [{
-        id: 1,
+        id: 6,
+        position: "Frontend Engineer",
+        startDate: "March 2022",
+        endDate: "June 2022",
+        company: "Kawan Kerja",
+        duration: 3,
+        workStatus: "Contract",
+        location: "Bandung, West Java",
+        description: "Contracted for developing elearning platform from scratch. Consist of homepage, course, mentor and consultation pages. Not only that, but also develop for admin pages, register, login, reset password ad update profiles. For the technology used in Frontend, React.js with Tailwind CSS for styling. And the language is TypeScript. Integration with backend using Axios HTTP. Work with agile. Sprint planning every monday, led by Project Manager and CTO, to determine the backlog of tasks that will be done for the next planning. During working, always communication with Project Manager, UI/UX Designer and Backend Developer during design till development process."
+    }, {
+        id: 5,
         position: "Frontend Engineer, Platform",
         startDate: "September 2021",
         endDate: "Maret 2022",
@@ -10,7 +20,7 @@ export const DataExperience = [{
         description: "Work with agile. Standup meeting every day to report on the tasks that have been done and the tasks that will be done that day with Project Manager. Build website from scratch like landing page, authentication page who contains register, login, and forgot password page. Build dashboard admin page from scratch to manage a content. Contains charts, tables, multipart form edit delete and recent activity admin. Also communication with Project Manager, UI/UX Designer and Backend Developer during design till development process."
     },
     {
-        id: 2,
+        id: 4,
         position: "Frontend Developer",
         startDate: "September 2021",
         endDate: "January 2022",
@@ -32,7 +42,7 @@ export const DataExperience = [{
         description: "ALA is a Startup who focused on bringing together farmers and vegetable buyers Customer will detect seller on near location and make a order. As a Lead Frontend Developer, my responsibility is forming a Frontend Team, Lead working UI and UX Design phase, create and push a Boilerplate Project(Next JS + Tailwind). Translated UI design to Web view(frontend) using Tailwind CSS. Review and clean the Code member on Branch Repository, make sure the Web showcase is SEO friendly using Next JS and will fast load assets like css module and image, finally collaboration with backend dev to integrate with Backend Service API."
     },
     {
-        id: 4,
+        id: 2,
         position: "Frontend Developer  |  UI/UX Designer",
         startDate: "March 2020",
         endDate: "October 2020",
@@ -43,7 +53,7 @@ export const DataExperience = [{
         description: "Karisma Academy is a Company IT Training and Software Agency. Im as a Frontend Developer, in this job my responsibility is research the problem, create User Experience Design ( Wireframe ), translate Wireframe to UI Design, translate UI Design to Code Frontend Website, communicate with Backend Developer about web progress. In other time,i also as a Instructor in Web Development Division.Specifically teach about Frontend Development, HTML CSS JavaScript, PHP and Adobe XD."
     },
     {
-        id: 5,
+        id: 1,
         position: "Frontend Developer  |  UI/UX Designer",
         startDate: "June 2019",
         endDate: "October 2019",
